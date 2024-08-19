b7ad6ebce821101f7fa1b351db6d5fc3c301b478
Basil-sajeev <basilsajeev360@gmail.com>
2024-08-19 11:25:07 +0530

    button

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.style.display = navLinks.style.display === 'block' ? 'none' : 'block';
+function myFunction() {
+  var book = document.getElementById("book");
+  if (book.style.display === "flex") {
+      book.style.display = "none";
+  } else {
+      book.style.display = "flex";
   }
+  }
+
+
+function myFunction1() {
+    var book1=document.getElementById("book1");
+    if(book1.style.display==="block"){
+      book1.style.display="none";
+    }
+    else {
+      book1.style.display = "block";
+    }
+}
+
+function myFunction2(){
+
+  var book2=document.getElementById("book2");
+    if(book2.style.display==="block"){
+      book2.style.display="none";
+    }
+    else {
+      book2.style.display = "block";
+    }
+
+}
+
+
+
+function toggleMenu(){
+  var head=document.getElementById("head1");
+  if(head.style.display==="flex"){
+    head.style.display="none";
+  }
+  else{
+    head.style.display="flex";
+  }
+  
+}
